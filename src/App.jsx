182276b5ff1aef93311c2cfbb1da7b2048f6a58a
Sanjana Gattraddy <2020ucs0092@iitjammu.ac.in>
2023-10-27 03:13:32 +0530

    added 4 image segments. Zoom left

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className='shipping centre'><p>FREE SHIPPING ON ALL ORDERS</p></div>
+    <div>
+    <div className='shipping'>
+    <p>FREE SHIPPING ON ALL ORDERS</p>
+    </div>
+
+    <div class="image-container1">
+    <img class="image1" src="https://thecoolmom.co/wp-content/uploads/2023/02/pexels-energepiccom-110473-1-scaled.jpg" alt="Image 1"></img>
+    <img class="image2" src="https://images.pexels.com/photos/261450/pexels-photo-261450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Image 2"></img>
+    </div>
+
+    <div class="image-container2">
+    <img class="image1" src="https://images.pexels.com/photos/753695/pexels-photo-753695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Image 1"></img>
+    <img class="image2" src="https://images.pexels.com/photos/5240857/pexels-photo-5240857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Image 2"></img>
+    </div>
+
+    <div class="image-container3">
+    {/* <img class="image1" src="https://images.pexels.com/photos/4348078/pexels-photo-4348078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Image 1"></img> */}
+    <img class="image2" src="https://images.unsplash.com/photo-1620298228682-e5eb972dfbe1?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image 2"></img>
+    </div>
+
+    <div class="image-container4">
+    <img class="image1" src="https://images.unsplash.com/photo-1633239894530-d715e7498078?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image 1"></img>
+    </div>
+    </div>
+    
+    
   );
 }
 
