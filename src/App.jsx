4b2c46ae6fb264089f9e7dddcdfe19147e165be7
Sanjana Gattraddy <2020ucs0092@iitjammu.ac.in>
2023-11-05 00:29:44 +0530

    made final changes. Hosted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+import axios from 'axios';
 
-function App() {
-
+export default function Elitefit() {
   const [hoveredElement, setHoveredElement] = useState(null);
+  const [check, setcheck] = useState(null);
 
   const handleMouseEnter = (elementNumber) => {
     setHoveredElement(elementNumber);
@@ -16,6 +15,12 @@ function App() {
   };
 
   const [data, setdata] = useState(null);
+  const scrollToAbout = () => {
+    const aboutSection = document.getElementById("about");
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   async function fetchData() {
     const url = 'http://localhost:3001/api/test-api/testimonials.json';
@@ -29,166 +34,150 @@ function App() {
       console.error('Error:', error);
     }
   }
+  const openSearch = () => {
+    // Update the check state to a non-null value
+    setcheck(!check); // You can replace 'SomeValue' with the desired non-null value
+  }
   useEffect(() => {
+
     fetchData()
   }, [data])
-
-
   return (
-    <div className='container'>
-      <div className='shipping container'>
-        <p>FREE SHIPPING ON ALL ORDERS</p>
-      </div>
+    <div className='container-fluid' style={{ width: "100%", height: "40px", backgroundColor: "black", gap: "10px" }}>
+      
+      {check && (<div style={{ borderRadius:"10px",display: "flex", justifyContent: "center", alignItems: "center", padding: "6px", position: "absolute", width: "500px", height: "70px", backgroundColor: "white", top: "20vh", right: "0%", gap: "15px" }}>
+        <textarea style={{ borderWidth: "px", borderRadius: "6px", width: "300px" }}></textarea>
+        <button style={{ backgroundColor: "black", color: "white", borderRadius: "10px", width: "100px", height: "40px" }}>Search</button>
 
-      <div style={{ justifyContent: "space-evenly", display: "flex", height: "83px", width: "1440px", padding: "9px", alignItems: "center" }}>
-        <p style={{ fontSize: "37px" }}>PISTONS</p>
+      </div>)
 
-        <div style={{ display: "flex", gap: "10px", fontWeight: "bold" }}>
-          <p>New</p>
-          <p>Brand</p>
-          <p>Collection</p>
-          <p>Accesories</p>
-          <p>About</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", width: "100px", gap: "5px" }}>
-          <div className='back1'></div>
-          <div className='back'></div>
-
+      }
+      <div className='row'>
+        <div className='col-sm-12 shipping '>
+          <p>FREE SHIPPING ON ALL ORDERS</p>
         </div>
       </div>
+      <div className="row">
 
-      <div class="image-container container">
-        <div className="image1 container" >
-
-          <p className='text1'>
-            MADE FOR YOU
-          </p>
-
+        <div className="col-sm-4" style={{ display: "flex", alignItems: "center" }}>
+          <p className="x2">PISTONS</p>
         </div>
-
-        <div className='image2 container'>
-          <div style={{ height: "176px", width: "370px", paddingTop: "388px", paddingLeft: "175px" }}>
-            <div className='text2'>
-              <p>
-                YOU WERE MADE TO MAKE AN IMPACT.
-              </p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-      <div class="image-container container">
-        <div className="image3 container" >
-          <div style={{ height: "176px", width: "370px", paddingTop: "388px", paddingLeft: "175px" }}>
-            <p className='text3'>
-              MADE FOR THE BEST CHANGE MAKERS
-            </p>
+        <div className="col-sm-4">
+          <div className="row" style={{ fontWeight: "bold", height: "100%" }}>
+            <div className="col-3 centy">New</div>
+            <div className="col-3 centy"><a style={{ color: "black", textDecoration: "none" }} href="#about" onClick={scrollToAbout}>
+              About
+            </a></div>
+            <div className="col-3 centy">Ascessories</div>
+            <div className="col-3 centy">Collection</div>
           </div>
         </div>
-
-
-        <div className='image4'>
-        </div>
-      </div>
-
-      <div class="image-container container">
-        <div className="image5" >
-
-        </div>
-
-        <div className='image6'>
-          <div style={{ height: "176px", width: "370px", paddingTop: "388px", paddingLeft: "175px" }}>
-            <p className='text6'>
-              SAVOUR THE PRESENT
-            </p>
+        <div className="col-sm-4">
+          <div className="align-icon" style={{ display: "flex", alignItems: "center" }}>
+            <button className="back" onClick={openSearch}></button>
+            <div className="back1"></div>
           </div>
-
         </div>
       </div>
-
-      <div class="image-container container">
-        <div className='image7'>
-          <div className='trans'>
-            <div>
-              <p className='text7'>
-                WHERE QUALITY MEETS CLASS.
-              </p>
-            </div>
-
-          </div>
-
-        </div>
+      <div className="row">
+        <div className="col-md-6 image1" style={{ height: "600px", backgroundColor: "grey" }}></div>
+        <div className="col-md-6 image2" style={{ height: "600px", backgroundColor: "black" }}></div>
+        <div className="col-md-6 image3" style={{ height: "600px", backgroundColor: "black" }}></div>
+        <div className="col-md-6 image4" style={{ height: "600px", backgroundColor: "grey" }}></div>
+        <div className="col-md-6 image5" style={{ height: "600px", backgroundColor: "black" }}></div>
+        <div className="col-md-6 image6" style={{ height: "600px", backgroundColor: "grey" }}></div>
       </div>
 
-
-
-      <div className="container" style={{ display: "flex", padding: "100px", width: "1440px" }}>
-        <div className="about" style={{ height: "436px", width: "620px" }}>
+      <div className="row container-fluid" style={{ padding: "100px" }}>
+        <div id="about" className="col-md-6 about" style={{ height: "600px" }}>
 
         </div>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "436px", width: "620px" }}>
-          <div>
-            <h1>
+        <div className="col-md-6 " style={{ fontSize: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+
+          <p style={{ width: "75%" }}>
+            <h1 >
               ABOUT  PISTONS
             </h1>
-            <p style={{ height: "201px", width: "443px" }}>
-              Established in 1897, our beloved fountain pen haven, 'PISTONS,' has been a haven for pen enthusiasts for generations. With a century-long dedication to fine writing instruments, we've bridged tradition and modernity, offering a curated selection of vintage and contemporary fountain pens. Our little shop, nestled in the heart of town, continues to inspire a love for the art of writing, connecting past and present through the timeless elegance of ink on paper.
-            </p>
-
-          </div>
-
+            Established in 1897, our beloved fountain pen haven, 'PISTONS,' has been a haven for pen enthusiasts for generations. With a century-long dedication to fine writing instruments, we've bridged tradition and modernity, offering a curated selection of vintage and contemporary fountain pens. Our little shop, nestled in the heart of town, continues to inspire a love for the art of writing, connecting past and present through the timeless elegance of ink on paper.
+          </p>
         </div>
       </div>
-      <div style={{ backgroundColor: "whitesmoke", width: "1440px", height: "552px", paddingTop: "25px", paddingLeft: "50px", paddingRight: "50px" }}>
-        <div style={{ textAlign: "center" }}>
+
+
+
+      <div style={{ backgroundColor: "whitesmoke", width: "100%", paddingTop: "25px", paddingLeft: "50px", paddingRight: "50px", }}>
+        <div style={{ textAlign: "center" }} >
           <h1>TESTIMONIALS</h1>
         </div>
-        <div style={{ marginTop: "100px", display: "flex", justifyContent: "space-evenly" }}>
+        <div className="row 10 " style={{ padding: "50px" }}>
 
-          {hoveredElement === 1 ? (
-            <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
-              <p style={{ width: "200px" }}>"{data[0]?.message}"</p>
-            </div>
-          ) : (
-            <div className="new1" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(1)}></div>
-          )}
-          {hoveredElement === 2 ? (
-            <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
-              <p style={{ width: "200px" }}>"{data[1]?.message}"</p>
-            </div>
-          ) : (
-            <div className="new2" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(2)}></div>
-          )}
-          {hoveredElement === 3 ? (
-            <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
-              <p style={{ width: "200px" }}>"{data[2]?.message}"</p>
-            </div>
-          ) : (
-            <div className="new3" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(3)}></div>
-          )}
-          {hoveredElement === 4 ? (
-            <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
-              <p style={{ width: "200px" }}>"{data[3]?.message}"</p>
-            </div>
-          ) : (
-            <div className="new4" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(4)}></div>
-          )}
+
+          <div className="col-xl-3" style={{ justifyContent: "center", display: "flex" }}>
+            {hoveredElement === 1 ? (
+              <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
+                <p style={{ width: "200px" }}>"{data[0]?.message}"</p>
+              </div>
+            ) : (
+              <div className="new1" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(1)}></div>
+            )}
+          </div>
+          <div className="col-xl-3" style={{ justifyContent: "center", display: "flex" }}>
+            {hoveredElement === 2 ? (
+              <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
+                <p style={{ width: "200px" }}>"{data[1]?.message}"</p>
+              </div>
+            ) : (
+              <div className="new2" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(2)}></div>
+            )}
+
+          </div>
+          <div className="col-xl-3" style={{ justifyContent: "center", display: "flex" }}>
+            {hoveredElement === 3 ? (
+              <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
+                <p style={{ width: "200px" }}>"{data[2]?.message}"</p>
+              </div>
+            ) : (
+              <div className="new3" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(3)}></div>
+            )}
+          </div>
+          <div className="col-xl-3" style={{ justifyContent: "center", display: "flex" }}>
+            {hoveredElement === 4 ? (
+              <div className="new11" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "5px", fontSize: "16px", color: "white", height: '277px', width: '277px', borderRadius: '15px' }} onMouseLeave={handleMouseLeave}>
+                <p style={{ width: "200px" }}>"{data[3]?.message}"</p>
+              </div>
+            ) : (
+              <div className="new4" style={{ height: '277px', width: '277px', borderRadius: '15px' }} onMouseEnter={() => handleMouseEnter(4)}></div>
+            )}
+          </div>
         </div>
+
       </div>
-      <div className='footer container'>
-        <div style={{ color: "white", position: "absolute", width: "151px", height: "62px", marginTop: "6px", marginLeft: "645px", fontSize: "30px" }}>
-          <p>PISTONS</p>
+      <div className='footer container-fluid'>
+        <div style={{ gap: "15px", display: "inline-grid"}}>
+          <div style={{ color: "white", width: "151px", height: "62px", fontSize: "35px" }}>
+            PISTONS
+          </div>
+          <div style={{ color: "white", fontSize: "28px", justifyContent: "center" }}>
+            STAY CONNECTED
+          </div>
+          <div style={{ display: "flex", gap: "20px" }}>
+            <div className='logo_size back2'></div>
+            <div className='logo_size back3'></div>
+            <div className='logo_size back4'></div>
+            <div className='logo_size back5'></div>
+            <div className='logo_size back6'></div>
+
+          </div>
         </div>
+
       </div>
-      <br></br>
+
+
+
+
+
+
+
     </div>
-
-
-
-
-
-  );
+  )
 }
-
-export default App;
